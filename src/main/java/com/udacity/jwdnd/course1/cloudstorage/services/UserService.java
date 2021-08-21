4b5ba9cce915597f2c2966f467b101a26cb9b43d
Motasem94 <amotasem.ahmed@gmail.com>
2021-08-22 01:22:35 +0200

    getUser with username added

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -30,4 +30,8 @@ public class UserService {
         String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
         return userMapper.insertUser(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstname(), user.getLastname()));
     }
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
+    }
 }
