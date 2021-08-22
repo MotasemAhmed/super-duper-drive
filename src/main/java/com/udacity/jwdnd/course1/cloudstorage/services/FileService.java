1c65570b3f752633b354f0e95125074817737060
Motasem94 <amotasem.ahmed@gmail.com>
2021-08-22 17:15:45 +0200

    file service

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,54 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.FileMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Service
+public class FileService {
+    private final UserMapper userMapper;
+    private final FileMapper fileMapper;
+
+    public FileService(UserMapper userMapper, FileMapper fileMapper) {
+        this.userMapper = userMapper;
+        this.fileMapper = fileMapper;
+    }
+
+    public File getFile(String filename) {
+        return fileMapper.getFile(filename);
+    }
+
+    public void addFile(MultipartFile multipartFile, String username) throws IOException {
+        InputStream fis = multipartFile.getInputStream();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = fis.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        byte[] fileData = buffer.toByteArray();
+
+        String fileName = multipartFile.getOriginalFilename();
+        String contentType = multipartFile.getContentType();
+        String fileSize = String.valueOf(multipartFile.getSize());
+        Integer userId = userMapper.getUser(username).getUserid();
+        File file = new File(0, userId, fileName, contentType, fileSize, fileData);
+        fileMapper.insertFile(file);
+    }
+
+    public List<File> getAllFiles(Integer userId) {
+        return fileMapper.getAllFiles(userId);
+    }
+
+    public void deleteFile(Integer fileid) {
+        fileMapper.deleteFile(fileid);
+    }
+}
