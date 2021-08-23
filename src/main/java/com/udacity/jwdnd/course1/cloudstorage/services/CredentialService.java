623a2b3a8a5e4e31ff09b7d531e459b783971300
Motasem94 <amotasem.ahmed@gmail.com>
2021-08-23 20:43:41 +0200

    Credential Service

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -0,0 +1,33 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CredentialService {
+
+    private final CredentialMapper credentialMapper;
+
+    public CredentialService(CredentialMapper credentialMapper) {
+        this.credentialMapper = credentialMapper;
+    }
+
+    public List<Credential> getCredentials(int userid) {
+        return credentialMapper.getCredentials(userid);
+    }
+
+    public int createCredential(Credential credential) {
+        return credentialMapper.insertCredential(credential);
+    }
+
+    public void updateCredential(Credential credential) {
+        credentialMapper.updateCredential(credential);
+    }
+
+    public void deleteCredential(int credentialId) {
+        credentialMapper.deleteCredential(credentialId);
+    }
+}
